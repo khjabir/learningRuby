b254f40a87a62caa24633f202a3122d2770900ad
Jabir <khjabir@gmail.com>
2016-03-09 14:14:05 +0530

    putting some comments is always helpful

--- 01_HelloWorld.rb
+++ 01_HelloWorld.rb
@@ -1,1 +1,3 @@
 puts "Hello World!"
+#so puts is used to display it in the terminal
+#also learned how to put a comment on ruby ;)
--- 02_HelloRuby.rb
+++ 02_HelloRuby.rb
@@ -3,4 +3,9 @@ def hello
 	puts out
 end
 
-hello
+#seems like the above one is a function definition
+#got a variable out and ask to display its value on the terninal
+
+hello #here it is calling the function
+
+#when I tried to call 'hello' before definition, it gave me an error.

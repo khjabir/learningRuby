36fd4659f69774be2e891b960b0b35f35b127699
Jabir <khjabir@gmail.com>
2016-03-09 16:29:28 +0530

    Now I don't want to put # in every line for making comments

---
+++ 06_comments.rb
@@ -0,0 +1,8 @@
+=begin
+
+I just knew that '#' will make the following words a comment.
+I was wondered how to make a multiline comment bacause '/**/' was not working.
+Now it's cleared.
+
+=end
+puts "This program contains multi-line comments"

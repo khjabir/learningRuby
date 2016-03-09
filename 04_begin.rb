8066b4692184f5286c547bfa88bda3a25195b619
Jabir <khjabir@gmail.com>
2016-03-09 15:33:32 +0530

    I can BEGIN something befor running my actual program

---
+++ 04_begin.rb
@@ -0,0 +1,12 @@
+puts "This is the main Ruby program"
+
+BEGIN {
+	puts "Initializing Ruby program"
+}
+
+# The output was:
+
+# Initializing Ruby program
+# This is the main Ruby program
+
+# So begin statement is used to declare code to be called before the program is run.

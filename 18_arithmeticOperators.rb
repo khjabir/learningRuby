dd547387ee108f04664682bfa393669fa35ee0a0
Jabir <khjabir@gmail.com>
2016-03-14 14:02:33 +0530

    intro to arithmetic operators

---
+++ 18_arithmeticOperators.rb
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+puts "a is 10 and b is 20"
+
+puts "a+b is #{a+b}"
+puts "a-b is #{a-b}"
+puts "a*b is #{a*b}"
+puts "b/a is #{b/a}"
+puts "b%a is #{b%a}"
+puts "a**b is #{a**b}"
+
+# Nothing more, Nothing less.. seems to be perfect..!!

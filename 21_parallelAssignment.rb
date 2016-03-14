80595f299aeaf903c591c06d53204960d67d6e59
Jabir <khjabir@gmail.com>
2016-03-14 15:12:18 +0530

    stunning parallel assignment

---
+++ 21_parallelAssignment.rb
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+
+print "initial values a = #{a}, b = #{b}"
+
+a,b = b,a
+
+print "\nafter swapping a = #{a}, b = #{b}\n"
+
+# One of the coolest feature of ruby...!!
+# Just loved it :)

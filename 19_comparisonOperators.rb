e62c7d0163a80ee84510742041bf97c55a6141ff
Jabir <khjabir@gmail.com>
2016-03-14 14:49:51 +0530

    intro to comparison operators

---
+++ 19_comparisonOperators.rb
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+
+puts "a is 10 and b is 20"
+
+puts "a==b is #{a==b}"
+puts "a!=b is #{a!=b}"
+puts "a>b is #{a>b}"
+puts "a<b is #{a<b}"
+puts "a>=b is #{a>=b}"
+puts "a<=b is #{a<=b}"
+puts "a<=>b is #{a<=>b}"
+puts "(1...10) === 5 is #{(1...10)===5}"
+puts "1.eql?(1.0) is #{1.eql?(1.0)}"
+puts "a.equal?b is #{a.equal?b}"
+
+# Up to the combined comparison operator, it seems to be OK..
+#remaining need more explanation!!

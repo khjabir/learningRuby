b59a177487841092ee75f1c13a93e8ccad91850d
Jabir <khjabir@gmail.com>
2016-03-14 12:13:19 +0530

    Range will rock

---
+++ 17_rangeInRuby.rb
@@ -0,0 +1,17 @@
+print "double dot: "
+
+(10..15).each do |n|
+	print n, ' '
+end
+
+puts
+
+print "Triple dot: "
+
+(10...15).each do |n|
+	print n, ' '
+end
+
+puts
+
+# Range is so cute..!! Triple dot ignores the last one..

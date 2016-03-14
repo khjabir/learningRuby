b5dcadea212a6d441a710fd56fb65be6ba4585c4
Jabir <khjabir@gmail.com>
2016-03-14 15:05:15 +0530

    an intro to assignment operators

---
+++ 20_assignmentOperators.rb
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+puts "a is 10 and b is 20"
+
+puts "c=a+b is #{c=a+b}"
+puts "c+=a is #{c+=a}"
+puts "c-=a is #{c-=a}"
+puts "c/=a is #{c/=a}"
+puts "a%=c is #{a%=c}"
+puts "c**b is #{c**=b}"
+
+# As usual, the abbreviated versions.

0c62bebaa88edfb90f5095294b5cafa8c453c013
Jabir <khjabir@gmail.com>
2016-03-15 10:29:45 +0530

    an intro to bitwise operators

---
+++ 22_bitwiseOperators.rb
@@ -0,0 +1,13 @@
+a = 60
+b = 13
+
+puts "a = #{a} and b = #{b}"
+puts "a&b = #{a&b}"
+puts "a|b = #{a|b}"
+puts "a^b = #{a^b}"
+puts "a&b = #{a&b}"
+puts "~a = #{~a}"
+puts "a<<2 = #{a<<2}"
+puts "a>>b = #{a>>2}"
+
+# Seems to be as simple as that..!!!

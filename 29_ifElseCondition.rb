498ffb9fb29f51015d5b6eefc0cd7853d3319494
Jabir <khjabir@gmail.com>
2016-03-15 15:09:35 +0530

    elsif seems to be a standard one

---
+++ 29_ifElseCondition.rb
@@ -0,0 +1,12 @@
+x = 1
+
+if x > 2
+	puts "x is greater than 2"
+elsif x <=2 and x != 0
+	puts "x is 1"
+else
+	puts "I can't guess the number"
+end
+
+# So if needs an end too.
+# it's 'elsif' not 'else if' nor 'elseif'

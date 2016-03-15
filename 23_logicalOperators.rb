aee9d8193fd61a17dc7f11bfc35b28dc805d8ecf
Jabir <khjabir@gmail.com>
2016-03-15 10:44:06 +0530

    an intro to logical operators

---
+++ 23_logicalOperators.rb
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+puts "a = #{a} and b = #{b}"
+
+puts "a and b = #{a and b}"
+puts "a or b = #{a or b}"
+puts "a && b = #{a && b}"
+puts "a || b = #{a || b}"
+puts "!(a && b) = #{!(a && b)}"
+puts "not(a && b) = #{not(a && b)}"
+
+# Why a and b is giving 20 instead of true???
